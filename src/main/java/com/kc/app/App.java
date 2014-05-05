a7b995d310bd61b3c89961b6c433cc357ff4def5
Ana María <lular1992@gmail.com>
2014-05-05 12:11:31 +0200

    base

---
+++ src/main/java/com/kc/app/App.java
@@ -0,0 +1,13 @@
+package com.kc.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
